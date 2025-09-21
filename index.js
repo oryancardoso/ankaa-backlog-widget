c125c8255f6149ac05e937f32f6887031e460537
oryancardoso <rayancardososilva@gmail.com>
2025-09-21 09:43:09 -0300

    index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// A LINHA MÁGICA QUE FALTAVA:
-// Esta linha ensina o servidor a encontrar QUALQUER arquivo (imagens, etc.) que está na mesma pasta.
-app.use(express.static(__dirname));
+// A LINHA DECISIVA:
+// Ensina o servidor a encontrar e entregar arquivos da pasta 'public'
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(3000, () => {
-  console.log('Servidor Ankaa HLTB Widget (Versão Final Corrigida) está no ar!');
+  console.log('Servidor Ankaa HLTB Widget (Versão Finalíssima) está no ar!');
 });
